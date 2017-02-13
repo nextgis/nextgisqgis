b0075bbd383cb685757676259a1f0c6ac001cb10
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-02-13 15:06:46 +0300

    Add remote python plugins

---
+++ scripts/process_repo_xml.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ET
+import sys
+import os
+from distutils.version import StrictVersion
+
+# arg[1] plugin name
+# arg[2] path to xml files
+
+plugin_name = sys.argv[1]
+repo_path = sys.argv[2]
+
+output_url = ''
+version = '0.0.0'
+
+
+# list all xml files
+for repo_xml in os.listdir(repo_path):
+    tree = ET.parse(os.path.join(repo_path, repo_xml))
+    root = tree.getroot()
+    for pyqgis_plugin in root.findall('pyqgis_plugin'):
+        if plugin_name == pyqgis_plugin.get('name'):
+            if StrictVersion(pyqgis_plugin.get('version')) > StrictVersion(version):
+                version = pyqgis_plugin.get('version')
+                output_url = pyqgis_plugin.find('download_url').text
+#                if not output_url.endswith('.zip'):
+#                    output_url += pyqgis_plugin.find('file_name').text
+
+print output_url,
