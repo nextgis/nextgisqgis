3199b3838c08ca2493a596d71841988d4ff39d9c
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-05-18 14:44:35 +0300

    Fix build on Windows

--- opt/overwrite/src/app/main.cpp
+++ opt/overwrite/src/app/main.cpp
@@ -779,10 +779,10 @@ int main( int argc, char *argv[] )
     putenv( QString("GDAL_DATA=" + QCoreApplication::applicationDirPath().append( "/../share/gdal" )).toUtf8().constData() );
     putenv( QString("GDAL_DRIVER_PATH=" + QCoreApplication::applicationDirPath().append( "/../" ) + QString( QGIS_PLUGIN_SUBDIR ) ).toUtf8().constData() );
     if(!getenv("SSL_CERT_FILE")) {
-        putenv(QString("SSL_CERT_FILE=" + QCoreApplication::applicationDirPath().append( "/../share/ssl/certs/cert.pem" )));
+        putenv(QString("SSL_CERT_FILE=" + QCoreApplication::applicationDirPath().append( "/../share/ssl/certs/cert.pem" )).toUtf8().constData());
     }
     if (!getenv("CURL_CA_BUNDLE")) {
-        putenv(QString("CURL_CA_BUNDLE=" + QCoreApplication::applicationDirPath().append( "/../share/ssl/certs/cert.pem" )));
+        putenv(QString("CURL_CA_BUNDLE=" + QCoreApplication::applicationDirPath().append( "/../share/ssl/certs/cert.pem" )).toUtf8().constData());
     }
 #endif // Q_OS_WIN
 
