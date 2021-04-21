a0db6e923cb0bc79bfd2f3fdd02d5633d971f7d4
d3n15 <x514ckw4r3@gmail.com>
2021-04-22 02:09:50 +0300

    QgsSvgCache::svgAsPicture(): make sure the returned picture is not shared
    From https://github.com/qgis/QGIS/commit/75b7edf1d211cd136fb54d2216cfea6e9fd2e120

--- src/core/symbology-ng/qgssvgcache.cpp
+++ src/core/symbology-ng/qgssvgcache.cpp
@@ -175,8 +175,12 @@ const QPicture& QgsSvgCache::svgAsPicture( const QString& file, double size, con
     trimToMaximumSize();
   }
 
-  QPicture p = *( currentEntry->picture );
-  p.detach();
+  QPicture p;
+  // For some reason p.detach() doesn't seem to always work as intended, at
+  // least with QT 5.5 on Ubuntu 16.04
+  // Serialization/deserialization is a safe way to be ensured we don't
+  // share a copy.
+  p.setData( currentEntry->picture->data(), currentEntry->picture->size() );
   return p;
 }
 
