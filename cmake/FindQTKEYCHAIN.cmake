d8c60d14587b76be7b5928dc56945b82f7a02aa7
Dmitry Baryshnikov <bishop.dev@gmail.com>
2022-04-19 22:37:25 +0300

    Fix biuld on linux

--- cmake/FindQTKEYCHAIN.cmake
+++ cmake/FindQTKEYCHAIN.cmake
@@ -14,39 +14,39 @@
 #    QTKEYCHAIN_INCLUDE_DIR
 #    QTKEYCHAIN_LIBRARY
 
-FIND_PATH(QTKEYCHAIN_INCLUDE_DIR keychain.h
-  PATHS
-  ${LIB_DIR}/include
-  "$ENV{LIB_DIR}/include"
-  $ENV{INCLUDE}
-  /usr/local/include
-  /usr/include
-  PATH_SUFFIXES qt5keychain qtkeychain
+find_path(QTKEYCHAIN_INCLUDE_DIR keychain.h
+    PATHS
+    ${LIB_DIR}/include
+    "$ENV{LIB_DIR}/include"
+    $ENV{INCLUDE}
+    /usr/local/include
+    /usr/include
+    PATH_SUFFIXES qt5keychain qtkeychain
 )
 
-FIND_LIBRARY(QTKEYCHAIN_LIBRARY NAMES qt5keychain qtkeychain
-  PATHS
-  ${LIB_DIR}
-  "$ENV{LIB_DIR}"
-  $ENV{LIB_DIR}/lib
-  $ENV{LIB}
-  /usr/local/lib
-  /usr/lib
+find_library(QTKEYCHAIN_LIBRARY NAMES qt5keychain qtkeychain
+    PATHS
+    ${LIB_DIR}
+    "$ENV{LIB_DIR}"
+    $ENV{LIB_DIR}/lib
+    $ENV{LIB}
+    /usr/local/lib
+    /usr/lib
 )
 
 
-IF (QTKEYCHAIN_INCLUDE_DIR AND QTKEYCHAIN_LIBRARY)
-  SET(QTKEYCHAIN_FOUND TRUE)
-ELSE()
-  SET(QTKEYCHAIN_FOUND FALSE)
-ENDIF (QTKEYCHAIN_INCLUDE_DIR AND QTKEYCHAIN_LIBRARY)
+if (QTKEYCHAIN_INCLUDE_DIR AND QTKEYCHAIN_LIBRARY)
+    set(QTKEYCHAIN_FOUND TRUE)
+else ()
+    set(QTKEYCHAIN_FOUND FALSE)
+endif ()
 
-IF (QTKEYCHAIN_FOUND)
-   IF (NOT QTKEYCHAIN_FIND_QUIETLY)
-      MESSAGE(STATUS "Found QtKeychain: ${QTKEYCHAIN_LIBRARY}")
-   ENDIF (NOT QTKEYCHAIN_FIND_QUIETLY)
-ELSE (QTKEYCHAIN_FOUND)
-   IF (QTKEYCHAIN_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find QtKeychain")
-   ENDIF (QTKEYCHAIN_FIND_REQUIRED)
-ENDIF (QTKEYCHAIN_FOUND)
+if (QTKEYCHAIN_FOUND)
+    if (NOT QTKEYCHAIN_FIND_QUIETLY)
+        MESSAGE(STATUS "Found QtKeychain: ${QTKEYCHAIN_LIBRARY}")
+    endif ()
+else ()
+    if (QTKEYCHAIN_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find QtKeychain")
+    endif ()
+endif ()
