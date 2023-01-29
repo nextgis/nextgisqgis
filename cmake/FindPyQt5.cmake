fd8d42d25a8273afd880cfee5f70b0a8aa8975fd
d3n15 <x514ckw4r3@gmail.com>
2023-01-29 10:34:02 +0300

    Fixed build on Linux Focal

--- cmake/FindPyQt5.cmake
+++ cmake/FindPyQt5.cmake
@@ -1,42 +1,68 @@
-################################################################################
-# Project:  NextGIS GIS libraries
-# Purpose:  CMake build scripts
-# Copyright (C) 2022, NextGIS <info@nextgis.com>
+# Find PyQt5
+# ~~~~~~~~~~
+# Copyright (c) 2007-2008, Simon Edwards <simon@simonzone.com>
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
-# This script is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+# PyQt5 website: http://www.riverbankcomputing.co.uk/pyqt/index.php
 #
-# This script is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Find the installed version of PyQt5. FindPyQt5 should only be called after
+# Python has been found.
 #
-# You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# This file defines the following variables:
+#
+# PYQT5_VERSION_STR - The version of PyQt5 as a human readable string.
+#
+# PYQT5_SIP_DIR - The directory holding the PyQt5 .sip files.
+#
+# PYQT5_SIP_FLAGS - The SIP flags used to build PyQt.
 
 IF(EXISTS PYQT5_VERSION_STR)
   # Already in cache, be silent
   SET(PYQT5_FOUND TRUE)
 ELSE(EXISTS PYQT5_VERSION_STR)
 
-  FIND_FILE(_find_pyqt5_py FindPyQt5.py PATHS ${CMAKE_MODULE_PATH} NO_CMAKE_FIND_ROOT_PATH)
+  IF(SIP_BUILD_EXECUTABLE)
+    # SIP >= 5.0 path
 
-  EXECUTE_PROCESS(COMMAND ${PYTHON_EXECUTABLE} ${_find_pyqt5_py} OUTPUT_VARIABLE pyqt_config)
-  IF(pyqt_config)
-    STRING(REGEX REPLACE ".*\npyqt_version_str:([^\n]+).*$" "\\1" PYQT5_VERSION_STR ${pyqt_config})
-    STRING(REGEX REPLACE ".*\npyqt_mod_dir:([^\n]+).*$" "\\1" PYQT5_MOD_DIR ${pyqt_config})
-    STRING(REGEX REPLACE ".*\npyqt_sip_dir:([^\n]+).*$" "\\1" PYQT5_SIP_DIR ${pyqt_config})
-    IF(EXISTS ${PYQT5_SIP_DIR}/Qt5)
-      SET(PYQT5_SIP_DIR ${PYQT5_SIP_DIR}/Qt5)
-    ENDIF(EXISTS ${PYQT5_SIP_DIR}/Qt5)
-    STRING(REGEX REPLACE ".*\npyqt_sip_flags:([^\n]+).*$" "\\1" PYQT5_SIP_FLAGS ${pyqt_config})
-    STRING(REGEX REPLACE ".*\npyqt_bin_dir:([^\n]+).*$" "\\1" PYQT5_BIN_DIR ${pyqt_config})
-    STRING(REGEX REPLACE ".*\npyqt_sip_module:([^\n]+).*$" "\\1" PYQT5_SIP_IMPORT ${pyqt_config})
-    SET(PYQT5_FOUND TRUE)
-  ENDIF(pyqt_config)
+    FILE(GLOB _pyqt5_metadata "${Python3_SITEARCH}/PyQt5-*.dist-info/METADATA")
+    IF(_pyqt5_metadata)
+      FILE(READ ${_pyqt5_metadata} _pyqt5_metadata_contents)
+      STRING(REGEX REPLACE ".*\nVersion: ([^\n]+).*$" "\\1" PYQT5_VERSION_STR ${_pyqt5_metadata_contents})
+    ELSE(_pyqt5_metadata)
+      EXECUTE_PROCESS(COMMAND ${Python3_EXECUTABLE} -c "from PyQt5.QtCore import PYQT_VERSION_STR; print(PYQT_VERSION_STR)" OUTPUT_VARIABLE PYQT5_VERSION_STR)
+    ENDIF(_pyqt5_metadata)
+
+    IF(PYQT5_VERSION_STR)
+      EXECUTE_PROCESS(COMMAND ${Python3_EXECUTABLE} -c "import PyQt5.QtCore; import os; print(os.path.dirname(PyQt5.__file__))" OUTPUT_VARIABLE PYQT5_MOD_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
+      SET(PYQT5_SIP_DIR "${PYQT5_MOD_DIR}/bindings")
+      
+      FIND_PROGRAM(__pyuic5 "pyuic5")
+      GET_FILENAME_COMPONENT(PYQT5_BIN_DIR ${__pyuic5} DIRECTORY)
+
+      SET(PYQT5_FOUND TRUE)
+    ENDIF(PYQT5_VERSION_STR)
+
+  ELSE(SIP_BUILD_EXECUTABLE)
+    # SIP 4.x path
+
+    FIND_FILE(_find_pyqt5_py FindPyQt5.py PATHS ${CMAKE_MODULE_PATH} NO_CMAKE_FIND_ROOT_PATH)
+
+    EXECUTE_PROCESS(COMMAND ${Python3_EXECUTABLE} ${_find_pyqt5_py} OUTPUT_VARIABLE pyqt_config)
+    IF(pyqt_config)
+      STRING(REGEX REPLACE "^pyqt_version_str:([^\n]+).*$" "\\1" PYQT5_VERSION_STR ${pyqt_config})
+      STRING(REGEX REPLACE ".*\npyqt_mod_dir:([^\n]+).*$" "\\1" PYQT5_MOD_DIR ${pyqt_config})
+      STRING(REGEX REPLACE ".*\npyqt_sip_dir:([^\n]+).*$" "\\1" PYQT5_SIP_DIR ${pyqt_config})
+      IF(EXISTS ${PYQT5_SIP_DIR}/Qt5)
+        SET(PYQT5_SIP_DIR ${PYQT5_SIP_DIR}/Qt5)
+      ENDIF(EXISTS ${PYQT5_SIP_DIR}/Qt5)
+      STRING(REGEX REPLACE ".*\npyqt_sip_flags:([^\n]+).*$" "\\1" PYQT5_SIP_FLAGS ${pyqt_config})
+      STRING(REGEX REPLACE ".*\npyqt_bin_dir:([^\n]+).*$" "\\1" PYQT5_BIN_DIR ${pyqt_config})
+      STRING(REGEX REPLACE ".*\npyqt_sip_module:([^\n]+).*$" "\\1" PYQT5_SIP_IMPORT ${pyqt_config})
+      SET(PYQT5_FOUND TRUE)
+    ENDIF(pyqt_config)
+
+  ENDIF(SIP_BUILD_EXECUTABLE)
 
   IF(PYQT5_FOUND)
     IF(NOT PyQt5_FIND_QUIETLY)
--- cmake/FindPyQt5.py
+++ cmake/FindPyQt5.py
@@ -1,78 +1,59 @@
-################################################################################
-# Project:  NextGIS GIS libraries
-# Purpose:  CMake build scripts
-# Copyright (C) 2022, NextGIS <info@nextgis.com>
+# -*- coding: utf-8 -*-
 #
-# This script is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+#   Copyright (c) 2007, Simon Edwards <simon@simonzone.com>
+#    All rights reserved.
 #
-# This script is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are met:
+#        * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#        * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#        * Neither the name of the  Simon Edwards <simon@simonzone.com> nor the
+#        names of its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
 #
-# You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+#    THIS SOFTWARE IS PROVIDED BY Simon Edwards <simon@simonzone.com> ''AS IS'' AND ANY
+#    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#    DISCLAIMED. IN NO EVENT SHALL Simon Edwards <simon@simonzone.com> BE LIABLE FOR ANY
+#    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# FindPyQt.py
+# Copyright (c) 2007, Simon Edwards <simon@simonzone.com>
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-import re
-import sys
-import os
-from distutils.sysconfig import get_python_lib
-
-try:
-    # On Windows and Python 3.8+ python doesn't load module DLL's
-    # from the current PATH environment, therefore we should add
-    # path to Qt's DLL's manually. This variable is passed from
-    # FindPyQt5.cmake
-    for path in os.environ['PYTHONDLLPATH'].split(';'):
-        os.add_dll_directory(path)
-except:
-    pass
-
+import os.path
 import PyQt5.QtCore
+import sipconfig
+import sys
 
-print("pyqt_version:%06.0x" % PyQt5.QtCore.PYQT_VERSION)
+cfg = sipconfig.Configuration()
+sip_dir = cfg.default_sip_dir
+for p in (os.path.join(sip_dir, "PyQt5"),
+          os.path.join(sip_dir, "PyQt5-3"),
+          sip_dir,
+          os.path.join(cfg.default_mod_dir, "PyQt5", "bindings")):
+    if os.path.exists(os.path.join(p, "QtCore", "QtCoremod.sip")):
+        sip_dir = p
+        break
+
 print("pyqt_version_str:%s" % PyQt5.QtCore.PYQT_VERSION_STR)
-
-pyqt_version_tag = ""
-in_t = False
-pyqt_config_list = PyQt5.QtCore.PYQT_CONFIGURATION["sip_flags"].split(' ')
-for item in pyqt_config_list:
-    if item == "-t":
-        in_t = True
-    elif in_t:
-        if item.startswith("Qt_5"):
-            pyqt_version_tag = item
-    else:
-        in_t = False
-print("pyqt_version_tag:%s" % pyqt_version_tag)
+print("pyqt_mod_dir:%s" % os.path.join(cfg.default_mod_dir, "PyQt5"))
+print("pyqt_sip_dir:%s" % sip_dir)
+print("pyqt_sip_flags:%s" % PyQt5.QtCore.PYQT_CONFIGURATION['sip_flags'])
+print("pyqt_bin_dir:%s" % cfg.default_bin_dir)
 
 try:
-    index_n = pyqt_config_list.index('-n')
-    pyqt_sip_name = pyqt_config_list[index_n + 1]
-    print("pyqt_sip_name:%s" % pyqt_sip_name)
-except ValueError:
-    pass
+    import PyQt5.sip
 
-# If we use a system install with a custom PYTHONPATH, PyQt5
-# may not be within the system tree.
-pyqt5_dir = os.path.dirname(PyQt5.__file__)
-lib_site_packages = get_python_lib(plat_specific=1)
-# Check this first.
-if pyqt5_dir.startswith(lib_site_packages):
-    pyqt_sip_dir = os.path.join(get_python_lib(plat_specific=1), "PyQt5", "bindings")
-else:
-    # If so, change it.
-    pyqt_sip_dir = os.path.join(pyqt5_dir, "bindings")
-
-if not os.path.exists(pyqt_sip_dir):  # Fallback for older PyQt5/SIP
-    pyqt_sip_dir = os.path.join(sys.prefix, "share", "sip", "PyQt5")
-print("pyqt_sip_dir:%s" % pyqt_sip_dir)
-
-print("pyqt_sip_flags:%s" % PyQt5.QtCore.PYQT_CONFIGURATION["sip_flags"])
-
-tags = re.findall(r"-t ([^\s]+)", PyQt5.QtCore.PYQT_CONFIGURATION["sip_flags"])
-print("pyqt_sip_tags:%s" % ",".join(tags))
+    print("pyqt_sip_module:PyQt5.sip")
+except:
+    print("pyqt_sip_module:sip")
